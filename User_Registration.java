152f2a001bb06e6a2323e56de3bd22b77a62a816
RuchikaJamode <ruchikasjamode@gmail.com>
2024-01-14 14:56:56 +0530

    last name valid or Not

--- User_Registration.java
+++ User_Registration.java
@@ -14,7 +14,7 @@ public class User_Registration {
             System.out.println("Invalid first name. Please ensure it starts with a capital letter and has at least 3 characters.");
         }
         
-        scanner.close();
+        //scanner.close();
     }
 
     private static boolean isValidFirstName(String firstName) {
@@ -24,11 +24,35 @@ public class User_Registration {
         // Use String.matches() to check if the input matches the regular expression
         return firstName.matches(regex);
     }
+    public static void UC2(){
+    	Scanner scanner = new Scanner(System.in);
+          System.out.println("Enter your last name: ");
+          String lastName = scanner.nextLine();
+          // Validate the last name
+          if (isValidLastName(lastName)) {
+              System.out.println("Valid last name: " + lastName);
+          } else {
+              System.out.println("Invalid last name. Please ensure it starts with a capital letter and has at least 3 characters.");
+          }
+          
+          scanner.close();
+      }
+
+      private static boolean isValidLastName(String lastName) {
+          // Regular expression for validating the last name
+          String regex = "^[A-Z][a-zA-Z]{2,}$";
+          
+          // Use String.matches() to check if the input matches the regular expression
+          return lastName.matches(regex);
+      }
+  
+    
 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 UC1();
+UC2();
 	}
 
 }
